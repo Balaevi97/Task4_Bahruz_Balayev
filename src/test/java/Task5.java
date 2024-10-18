ed4f0ad51381051ecb25ddaec5fdd2fe3638eb7f
Bahruz Balaevi <bbalaevi@credo.ge>
2024-10-18 22:48:25 +0400

    Task5_commit2

--- src/test/java/Task5.java
+++ src/test/java/Task5.java
@@ -4,16 +4,24 @@ public class Task5 {
     public static void main(String[] args) {
         Random Random = new Random();
         int RandomNumber = Random.nextInt(50)+1;
-        System.out.println("Random number is " + RandomNumber);
+      //  System.out.println("Random number is " + RandomNumber);
 
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Please enter number 1 to 50 ");
         int costumerAssume = scanner.nextInt();
-        if (costumerAssume == RandomNumber) {
-            System.out.println("Congratulation, you get it!");
-        } else {
-            System.out.println("Unfortunately, you did not get it!");
-        }
+
+     while (true) {
+         if (costumerAssume == RandomNumber) {
+             System.out.println("Congratulation, you get it!");
+             break;
+         } else if (costumerAssume < RandomNumber) {
+             System.out.println("Please enter higher number");
+             costumerAssume = scanner.nextInt();
+         } else {
+             System.out.println("Please enter lower number");
+             costumerAssume = scanner.nextInt();
+         }
+     }
+
     }
 }
