f31a53ecf68acafd46afb707146300e9c58c05d4
Bahruz Balaevi <bbalaevi@credo.ge>
2024-10-18 22:32:18 +0400

    Task3_commit2

--- src/test/java/Task3.java
+++ src/test/java/Task3.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-        int input = 5;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("გთხოვთ შეიყვანოთ დადებითი რიცხვი: ");
+        int input = scanner.nextInt();
 
         for (int i = 1; i < 11; i++) {
             System.out.println(" 5 * " + i + " = " + input * i);
